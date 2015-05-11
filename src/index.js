b083007d6e090ea04fc98c6069e2f6f5ca6b7ae8
James Thompson <james.thompson@thoughtworks.com>
2015-05-11 22:20:06 +0100

    Made load automatically update the tree view

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ var generateImage = require("../generate-image");
 
 var $textarea = document.getElementById("text-area");
 
+var updateTreeView = function () {
+    var root = parser.parse($textarea.value);
+    generateImage(root);
+};
+
 document.getElementById("load").addEventListener("click", function () {
     $textarea.value = dialogs.openDialog();
+    updateTreeView();
 });
 
 document.getElementById("save").addEventListener("click", function () {
     dialogs.saveDialog($textarea.value);
 });
 
-document.getElementById("generate").addEventListener("click", function () {
-    var root = parser.parse($textarea.value);
-    generateImage(root);
-});
+document.getElementById("generate").addEventListener("click", updateTreeView);
 
