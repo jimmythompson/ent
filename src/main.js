af7fb15622c557471450f4303dbec41184b8a6e4
James Thompson <james.thompson@thoughtworks.com>
2015-05-11 19:51:34 +0100

    Added menu

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 var app = require('app');
+var Menu = require('menu');
 var BrowserWindow = require('browser-window');
+
+var menuTemplate = require('./menu');
 
 // Keep a global reference of the window object
 var mainWindow = null;
@@ -9,11 +12,12 @@ app.on('window-all-closed', function () {
 });
 
 app.on('ready', function () {
+    Menu.setApplicationMenu(Menu.buildFromTemplate(
+        menuTemplate));
+
     mainWindow = new BrowserWindow({width: 1024, height: 800});
 
     mainWindow.loadUrl('file://' + __dirname + '/views/index.html');
-
-    mainWindow.toggleDevTools();
 
     mainWindow.on('closed', function () {
         mainWindow = null;
---
+++ src/menu.js
@@ -0,0 +1,124 @@
+var BrowserWindow = require('browser-window');
+
+module.exports = [
+    {
+        label: 'Ent',
+        submenu: [
+            {
+                label: 'About Ent',
+                selector: 'orderFrontStandardAboutPanel:'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Services',
+                submenu: []
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Hide Ent',
+                accelerator: 'Command+H',
+                selector: 'hide:'
+            },
+            {
+                label: 'Hide Others',
+                accelerator: 'Command+Shift+H',
+                selector: 'hideOtherApplications:'
+            },
+            {
+                label: 'Show All',
+                selector: 'unhideAllApplications:'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Quit',
+                accelerator: 'Command+Q',
+                click: function() { app.quit(); }
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                label: 'Undo',
+                accelerator: 'Command+Z',
+                selector: 'undo:'
+            },
+            {
+                label: 'Redo',
+                accelerator: 'Shift+Command+Z',
+                selector: 'redo:'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Cut',
+                accelerator: 'Command+X',
+                selector: 'cut:'
+            },
+            {
+                label: 'Copy',
+                accelerator: 'Command+C',
+                selector: 'copy:'
+            },
+            {
+                label: 'Paste',
+                accelerator: 'Command+V',
+                selector: 'paste:'
+            },
+            {
+                label: 'Select All',
+                accelerator: 'Command+A',
+                selector: 'selectAll:'
+            }
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            {
+                label: 'Reload',
+                accelerator: 'Command+R',
+                click: function() { BrowserWindow.getFocusedWindow().reloadIgnoringCache(); }
+            },
+            {
+                label: 'Toggle DevTools',
+                accelerator: 'Command+0',
+                click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); }
+            }
+        ]
+    },
+    {
+        label: 'Window',
+        submenu: [
+            {
+                label: 'Minimize',
+                accelerator: 'Command+M',
+                selector: 'performMiniaturize:'
+            },
+            {
+                label: 'Close',
+                accelerator: 'Command+W',
+                selector: 'performClose:'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Bring All to Front',
+                selector: 'arrangeInFront:'
+            }
+        ]
+    },
+    {
+        label: 'Help',
+        submenu: []
+    }
+];
